6c6e063573cbb8c41b7baae657f439d98c7f84d7
hjgudoal <hjgudoal@gmail.com>
2024-09-11 17:51:59 +0000

    feat: initiated the project

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
-    constructor()ERC20("ha","ha"){} 
+    constructor()ERC20("git add . && git commit -m "feat: initiated the project" && git push origin main","chmod +x hihi.sh && ./hihi.sh"){} 
 
     function mint100tokens() public {
         _mint(msg.sender, 100*10**18);
